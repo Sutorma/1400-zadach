8d24c29a0f5d7a13446722d0f39cccb841c5bb79
Gera Sutormin <georgsutomin8@gmail.com>
2026-01-22 10:01:08 +0300

    1400ZADACH

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main 
+    public static void main(String[] args) { 
+// Задача 1.7: Вывести три числа одно под другим
+System.out.println("5\n10\n21");
+
+// Задача 1.3: Вывод с точностью (число e)
+
+System.out.printf("%.2f%n", 2.71828);
+
+// Выведет: 123.46;
+}
